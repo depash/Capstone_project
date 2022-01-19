9112a8a49decefffc917c398f18cd5d7e0550299
depash <depash2003@gmail.com>
2022-01-19 14:10:38 -0500

    added starter

---
+++ react-app/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import LoginForm from './components/auth/LoginForm';
+import SignUpForm from './components/auth/SignUpForm';
+import NavBar from './components/NavBar';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import UsersList from './components/UsersList';
+import User from './components/User';
+import { authenticate } from './store/session';
+import Channels from './components/channels';
+import Server from './components/server';
+
+function App() {
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      await dispatch(authenticate());
+      setLoaded(true);
+    })();
+  }, [dispatch]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path='/login' exact={true}>
+          <LoginForm />
+        </Route>
+        <Route path='/sign-up' exact={true}>
+          <SignUpForm />
+        </Route>
+        <ProtectedRoute path='/users' exact={true} >
+          <UsersList />
+        </ProtectedRoute>
+        <ProtectedRoute path='/users/:userId' exact={true} >
+          <User />
+        </ProtectedRoute>
+        <ProtectedRoute path='/' exact={true} >
+          <h1>My Home Page</h1>
+        </ProtectedRoute>
+        <Route path='/channels/:serverId'>
+          <Server />
+        </Route>
+        <Route path='/channels/:serverId/:channelId' exact={true}>
+          <Channels />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/channels.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+const Channels = () => {
+    return (
+        <h1>Chennalasdasd</h1>
+    )
+}
+
+export default Channels;
